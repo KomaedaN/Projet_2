db93abe8f32d74d3512b941cba0422da81e5219f
Thomas <84379061+KomaedaN@users.noreply.github.com>
2022-05-05 21:48:46 +0200

    update "book_page_focus()"

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def extract_product_information(elements,i):
+def extract_product_information(elements):
     result = []
     for element in elements:
         result.append(element.string)
-    print(result[i])
-    return result[i]
+    print(result)
+    return result
 
 
 def book_page_focus():
@@ -17,20 +17,24 @@ def book_page_focus():
 
     soup = BeautifulSoup(page, "html.parser")
 
+    book_product = soup.select_one("tr:nth-child(1) > td")
     book_title = soup.find_all("h1")
-    book_product = soup.find_all("td")
-    book_description = soup.find_all("p")
-    book_category = soup.find_all("a")
-    #book_rating = soup.find_all("p", "star-rating")
+    book_price_including_tax = soup.select_one("tr:nth-child(4) > td")
+    book_price_excluding_tax = soup.select_one("tr:nth-child(3) > td")
+    book_number_available = soup.select_one("tr:nth-child(6) > td")
+    book_description = soup.select_one("article > p")
+    book_category = soup.select_one("li:nth-child(3) > a")
+    #book_rating = soup.select_one("tr:nth-child(1) > td")
 
-    universal_product_code = extract_product_information(book_product, 0)
-    title = extract_product_information(book_title, 0)
-    price_including_tax = extract_product_information(book_product, 3)
-    price_excluding_tax = extract_product_information(book_product, 2)
-    number_available = extract_product_information(book_product, 5)
-    product_description = extract_product_information(book_description, 3)
-    category = extract_product_information(book_category, 3)
-    #review_rating = extract_product_information(book_rating, 0)
+
+    universal_product_code = extract_product_information(book_product)
+    title = extract_product_information(book_title)
+    price_including_tax = extract_product_information(book_price_including_tax)
+    price_excluding_tax = extract_product_information(book_price_excluding_tax)
+    number_available = extract_product_information(book_number_available)
+    product_description = extract_product_information(book_description)
+    category = extract_product_information(book_category)
+    #review_rating = extract_product_information(book_rating)
 
 
 book_page_focus()
